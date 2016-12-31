0da9cef83c3506e2aa98c9a25312f1d752cf5e2c
tanishi109 <hayakawa@goodpatch.com>
2016-12-31 13:46:11 +0900

    :sparkles: render segment

---
+++ app/scripts/app.js
@@ -0,0 +1,36 @@
+let ctx = null;
+
+$(() => {
+  const canvas = document.getElementById("stage");
+  ctx = canvas.getContext("2d");
+
+  new Segment(100, 50);
+});
+
+class Segment {
+  constructor(width, height, color = "#fff") {
+    this.x = height / 2;
+    this.y = height / 2;
+    this.width = width;
+    this.height = height;
+
+    this.init();
+  }
+
+  init() {
+    ctx.beginPath();
+
+    ctx.rect(this.x, this.y, this.width, this.height);  
+    ctx.stroke(); 
+
+    ctx.beginPath();
+    ctx.arc(this.x + this.height / 2, this.y + this.height / 2, 2, 0, Math.PI * 2, true);   
+    ctx.stroke(); 
+
+    ctx.beginPath();
+    ctx.arc(this.x + this.width - this.height / 2, this.y + this.height / 2, 2, 0, Math.PI * 2, true);
+    ctx.stroke(); 
+
+  }
+}
+
