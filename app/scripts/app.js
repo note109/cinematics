145de960f113c353c9af9e3423cd5ae472c2401e
tanishi109 <hayakawa@goodpatch.com>
2016-12-31 23:01:39 +0900

    :shirt: fix for lint. add jsdocs & shorten one line.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -7,7 +7,16 @@ $(() => {
   new Segment(100, 50);
 });
 
+/**
+  Create segment
+*/
 class Segment {
+  /**
+    Initialize parameters
+    @param {number} width
+    @param {number} height
+    @param {string} color
+  */
   constructor(width, height, color = '#fff') {
     this.x = height / 2;
     this.y = height / 2;
@@ -15,20 +24,27 @@ class Segment {
     this.height = height;
 
     this.init();
-}
+  }
 
+  /**
+    Draw segment
+  */
   init() {
     ctx.beginPath();
 
     ctx.rect(this.x, this.y, this.width, this.height);
     ctx.stroke();
 
-    ctx.beginPath();
-    ctx.arc(this.x + this.height / 2, this.y + this.height / 2, 2, 0, Math.PI * 2, true);
-    ctx.stroke();
+    const halfHeight = this.height / 2;
 
     ctx.beginPath();
-    ctx.arc(this.x + this.width - this.height / 2, this.y + this.height / 2, 2, 0, Math.PI * 2, true);
+    ctx.arc(this.x + halfHeight, this.y + halfHeight, 2, 0, Math.PI * 2, true);
+    ctx.stroke();
+
+    const rightXPos = this.x + this.width - halfHeight;
+
+    ctx.beginPath();
+    ctx.arc(rightXPos, this.y + halfHeight, 2, 0, Math.PI * 2, true);
     ctx.stroke();
   }
 }
